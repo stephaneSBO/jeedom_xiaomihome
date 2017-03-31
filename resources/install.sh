2e487d1cc9e9c07d27b24b87dbb9868ed39b9a32
sarakha63 <sarakha63@DESKTOP-D9IGIEV>
2017-03-31 11:41:23 +0200

    removed unused dependancy

--- resources/install.sh
+++ resources/install.sh
@@ -4,15 +4,13 @@ echo "Début de l'installation"
 
 echo 0 > /tmp/xiaomihome_dep
 
-sudo apt-get -y install python-pip python3-pip libffi-dev libssl-dev
+sudo apt-get -y install python-pip libffi-dev libssl-dev
 
 echo 70 > /tmp/xiaomihome_dep
-sudo pip install yeecli
-sudo pip install mihome
-sudo pip3 install python-mirobo
-echo 80 > /tmp/xiaomihome_dep
 
 sudo pip install future
+
+echo 80 > /tmp/xiaomihome_dep
 sudo pip install pycrypto
 
 echo 90 > /tmp/xiaomihome_dep
