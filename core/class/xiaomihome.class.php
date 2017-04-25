989303e3dd03ee862f0df8120fcafa854e9e783a
Cédric Dupont <lunarok@gmail.com>
2017-04-25 19:35:19 +0200

    ping without external command

--- core/class/xiaomihome.class.php
+++ core/class/xiaomihome.class.php
@@ -517,15 +517,25 @@ class xiaomihome extends eqLogic {
         }
     }
 
-    public function pingHost($ip) {
-        exec("sudo ping -c1 " . $ip, $output, $return_var);
-        if ($return_var != 0) {
-            $this->checkAndUpdateCmd('online', 0);
-            return false;
-        } else {
+    public function pingHost ($host, $timeout = 1) {
+        /* ICMP ping packet with a pre-calculated checksum */
+        $package = "\x08\x00\x7d\x4b\x00\x00\x00\x00PingHost";
+        $socket = socket_create(AF_INET, SOCK_RAW, 1);
+        socket_set_option($socket, SOL_SOCKET, SO_RCVTIMEO, array('sec' => $timeout, 'usec' => 0));
+        socket_connect($socket, $host, null);
+
+        $ts = microtime(true);
+        socket_send($socket, $package, strLen($package), 0);
+        if (socket_read($socket, 255)) {
+            $result = microtime(true) - $ts;
             $this->checkAndUpdateCmd('online', 1);
-            return true;
+        } else {
+            $result = false;
+            $this->checkAndUpdateCmd('online', 0);
         }
+        socket_close($socket);
+
+        return $result;
     }
 }
 
