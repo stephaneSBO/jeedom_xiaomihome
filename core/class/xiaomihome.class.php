4982d5df5b3846ba23cd4bbec41bdc605dc9db46
Cédric Dupont <lunarok@gmail.com>
2017-04-25 20:39:59 +0200

    working ping

--- core/class/xiaomihome.class.php
+++ core/class/xiaomihome.class.php
@@ -522,21 +522,14 @@ class xiaomihome extends eqLogic {
     }
 
     public function pingHost ($host, $timeout = 1) {
-            /* ICMP ping packet with a pre-calculated checksum */
-        $package = "\x08\x00\x7d\x4b\x00\x00\x00\x00PingHost";
-        $socket  = socket_create(AF_INET, SOCK_RAW, 1);
-        socket_set_option($socket, SOL_SOCKET, SO_RCVTIMEO, array('sec' => $timeout, 'usec' => 0));
-        socket_connect($socket, $host, null);
-        $ts = microtime(true);
-        socket_send($socket, $package, strLen($package), 0);
-        if (socket_read($socket, 255)) {
-            $result = microtime(true) - $ts;
+        exec("ping -c1 " . $host, $output, $return_var);
+        if ($return_var == 0) {
+            $result = true;
             $this->checkAndUpdateCmd('online', 1);
         } else {
             $result = false;
             $this->checkAndUpdateCmd('online', 0);
         }
-        socket_close($socket);
         return $result;
     }
 }
