5a76d6effea3def562c33a033162831ed9aa10fc
Cédric <cedric@hystou.wayang.fr>
2017-05-17 20:21:03 +0200

    iadd log in install script

--- resources/install.sh
+++ resources/install.sh
@@ -3,21 +3,24 @@ touch /tmp/xiaomihome_dep
 echo "Début de l'installation"
 
 echo 0 > /tmp/xiaomihome_dep
-
+echo "Installation des dépendances apt"
 sudo apt-get -y install python-pip libffi-dev libssl-dev
 
 echo 60 > /tmp/xiaomihome_dep
 
 if [ $(pip list | grep future | wc -l) -eq 0 ]; then
+    echo "Installation du module future pour python"
     sudo pip install future
 fi
 
 echo 70 > /tmp/xiaomihome_dep
-if [ $(pip list | grep pycrypto | wc -l) -eq 0 ]; then 
+if [ $(pip list | grep pycrypto | wc -l) -eq 0 ]; then
+    echo "Installation du module pycrypto pour python"
     sudo pip install pycrypto
 fi
 echo 80 > /tmp/xiaomihome_dep
 if [ $(pip list | grep construct | wc -l) -eq 0 ]; then
+    echo "Installation du module construct pour python"
     sudo pip install construct
 fi
 
