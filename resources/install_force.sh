fe243020a402892319197168239e01c9991d0a05
Cédric Dupont <lunarok@gmail.com>
2017-07-08 23:24:35 +0200

    force argument

--- resources/install_force.sh
+++ resources/install_force.sh
@@ -7,13 +7,13 @@ echo 0 > /tmp/xiaomihome_dep
 sudo apt-get -y install python-pip libffi-dev libssl-dev python-cryptography
 
 echo 60 > /tmp/xiaomihome_dep
-sudo pip install future --force-reinstall
+sudo pip install future --upgrade --ignore-installed
 
 echo 70 > /tmp/xiaomihome_dep
-sudo pip install pycrypto --force-reinstall
+sudo pip install pycrypto --upgrade --ignore-installed
 
 echo 80 > /tmp/xiaomihome_dep
-sudo pip install construct --force-reinstall
+sudo pip install construct --upgrade --ignore-installed
 
 echo 90 > /tmp/xiaomihome_dep
 
