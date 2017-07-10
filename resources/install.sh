d0af59c279b2e26f8327ff14a604a8b812378735
Cédric Dupont <lunarok@gmail.com>
2017-07-10 15:35:18 +0200

    Update install.sh

--- resources/install.sh
+++ resources/install.sh
@@ -6,6 +6,24 @@ echo 0 > /tmp/xiaomihome_dep
 echo "Installation des dépendances apt"
 sudo apt-get -y install python-pip libffi-dev libssl-dev python-cryptography
 
+echo 30 > /tmp/xiaomihome_dep
+if [ $(pip list | grep pyudev | wc -l) -eq 0 ]; then
+    echo "Installation du module pyudev pour python"
+    sudo pip install pyudev
+fi
+
+echo 40 > /tmp/xiaomihome_dep
+if [ $(pip list | grep requests | wc -l) -eq 0 ]; then
+    echo "Installation du module requests pour python"
+    sudo pip install requests
+fi
+
+echo 50 > /tmp/xiaomihome_dep
+if [ $(pip list | grep pyserial | wc -l) -eq 0 ]; then
+    echo "Installation du module pyserial pour python"
+    sudo pip install pyserial
+fi
+
 echo 60 > /tmp/xiaomihome_dep
 if [ $(pip list | grep future | wc -l) -eq 0 ]; then
     echo "Installation du module future pour python"
