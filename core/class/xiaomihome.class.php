3359805a1274edbfe0065a6ad22f84b2bf7a4b1b
Cédric <cedric@hystou.wayang.fr>
2017-07-17 22:42:00 +0200

    bugfix name + no creation of unknown

--- core/class/xiaomihome.class.php
+++ core/class/xiaomihome.class.php
@@ -81,6 +81,10 @@ class xiaomihome extends eqLogic {
                 if (!is_array($device)) {
                     return true;
                 }
+                if (!isset($device['model'])) {
+                  log::add('xiaomihome', 'debug', 'Modèle non reconnu dans les définitions : ' . $_def['model']);
+                    return true;
+                }
                 $xiaomihome = new xiaomihome();
                 $xiaomihome->setEqType_name('xiaomihome');
                 $xiaomihome->setLogicalId($logical_id);
